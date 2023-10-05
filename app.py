66dc5ba8aa86833339bfcc1f93e4257746044bda
Gazzamac10 <mccarthygary@hotmail.com>
2023-10-05 11:09:49 +0100

    v1

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="My Streamlit App", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded")
+
+st.title("Gary McCarthy")
+
+st.markdown("<h2>Automated BIM Data Validation <br>using Machine Learning</h2>", unsafe_allow_html=True)
