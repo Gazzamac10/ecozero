decdaef4bae9059c25ab7218f7e89688040b55dd
Gazzamac10 <mccarthygary@hotmail.com>
2023-10-05 23:07:28 +0100

    v1

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="My Streamlit App", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded")
 
-st.title("Gary McCarthy")
-
-st.markdown("<h2>Testing</h2>", unsafe_allow_html=True)
+st.title("EcoZero")
+st.image('MV5BNzlhYjEzOGItN2MwNS00ODRiLWE5OTItYThiNmJlMTdmMzgxXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg')
