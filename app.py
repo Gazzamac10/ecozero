f2324f024883e0bdec684ea607dec22a256ec106
Gazzamac10 <mccarthygary@hotmail.com>
2023-10-05 12:30:14 +0100

    v1

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="My Streamlit App", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded")
 
-st.title("Gary McCarthy")
+#st.title("Gary McCarthy")
 
-st.markdown("<h2>Testing</h2>", unsafe_allow_html=True)
+#st.markdown("<h2>Testing</h2>", unsafe_allow_html=True)
 
