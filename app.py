cdd82243043537a6b9d20c3f6b67a50c7b9733a9
Gazzamac10 <mccarthygary@hotmail.com>
2023-10-10 16:47:19 +0100

    v1

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.set_page_config(page_title="My Streamlit App", page_icon=":rocket:", layout="wide", initial_sidebar_state="expanded")
 
-st.title("EcoZero")
+st.title("EcoZerotesting")
 st.image('MV5BNzlhYjEzOGItN2MwNS00ODRiLWE5OTItYThiNmJlMTdmMzgxXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_.jpg')
