0283ae04759432992f8c09da8f2e5cc634f2152b
Gazzamac10 <mccarthygary@hotmail.com>
2023-10-10 18:04:45 +0100

    v1

--- tools/graph_maker.py
+++ tools/graph_maker.py
@@ -1,5 +1,4 @@
 import plotly.express
-from tools import ifchelper
 from matplotlib import pyplot as plt
 
 
@@ -20,95 +19,6 @@ style = {
     "xtick.labelsize": 18,
     "ytick.labelsize": 18,
 }
-
-def get_elements_graph(file):
-    types = ifchelper.get_types(file, "IfcBuildingElement")
-    types_count = ifchelper.get_type_occurence(file, types)
-    x_values, y_values = ifchelper.get_x_and_y(types_count)
-
-    plt.rcParams.update(style)
-    fig, ax = plt.subplots()
-    ax.bar(x_values, y_values, width=0.5, align="center", color="blue", alpha=0.5)
-    ax.set_title("Building Objects Count")
-    ax.tick_params(color="blue", rotation=90, labelsize="7", labelcolor="blue")
-    ax.tick_params(axis="x", rotation=90)
-    ax.set_xlabel("Element Class")
-    ax.set_ylabel("Count")
-    ax.xaxis.label.set_color("blue")
-    ax.yaxis.label.set_color("blue")
-
-    ax.set_box_aspect(aspect=1 / 2)
-    ax.axis()
-    # ax.xticks(y_pos, objects, rotation=90, size=10)
-    return ax.figure
-
-def get_elements_graph2(df,x,y):
-    #types = ifchelper.get_types(file, "IfcBuildingElement")
-    #types_count = ifchelper.get_type_occurence(file, types)
-    x_values, y_values = df[x],df[y]
-
-    plt.rcParams.update(style)
-    fig, ax = plt.subplots()
-    ax.bar(x_values, y_values, width=1, align="center", color="red", alpha=0.5)
-    #ax.set_title("Building Objects Count")
-    ax.tick_params(color="white", rotation=90, labelsize="7", labelcolor="blue")
-    ax.tick_params(axis="x", rotation=90)
-    ax.set_xlabel("Family Type")
-    ax.set_ylabel("Count")
-    ax.xaxis.label.set_color("white")
-    ax.yaxis.label.set_color("white")
-
-    ax.set_box_aspect(aspect=0.5)
-    ax.axis()
-    # ax.xticks(y_pos, objects, rotation=90, size=10)
-    return ax.figure
-
-
-def get_high_frequency_entities_graph(file):
-    types = ifchelper.get_types(file)
-    types_count = ifchelper.get_type_occurence(file, types)
-    x_values, y_values = ifchelper.get_x_and_y(types_count, 400)
-
-    plt.rcParams.update(style)
-    fig, ax = plt.subplots()
-    ax.bar(x_values, y_values, width=0.5, align="center", color="blue", alpha=0.5)
-
-    ax.set_title("IFC entity types frequency")
-
-    ax.tick_params(color="blue", rotation=90, labelsize="7", labelcolor="blue")
-    ax.tick_params(axis="x", rotation=90)
-    ax.set_xlabel("File Entities")
-    ax.set_ylabel("No of occurences")
-    ax.xaxis.label.set_color("blue")
-    ax.yaxis.label.set_color("blue")
-
-    ax.set_box_aspect(aspect=1 / 2)
-    ax.axis()
-    # ax.xticks(y_pos, objects, rotation=90, size=10)
-    return ax.figure
-
-def get_high_frequency_entities_graph2(file,x,y):
-    x_values, y_values = file[x],file[y]
-
-    plt.rcParams.update(style)
-    fig, ax = plt.subplots()
-    ax.bar(x_values, y_values, width=1, align="center", color="Red", alpha=0.5)
-
-    #ax.set_title("IFC")
-
-    ax.tick_params(color="blue", rotation=90, labelsize="16", labelcolor="blue")
-    ax.tick_params(axis="x", rotation=90)
-    for i, v in enumerate(y_values):
-        ax.text(i, v + 0.1, str(v), color='blue', ha='center', fontsize=12)
-    ax.set_xlabel("Family Types")
-    ax.set_ylabel("No of occurences")
-    ax.xaxis.label.set_color("White")
-    ax.yaxis.label.set_color("White")
-
-    ax.set_box_aspect(aspect= 0.5)
-    ax.axis()
-    # ax.xticks(y_pos, objects, rotation=90, size=10)
-    return ax.figure
 
 
 def load_pie(dataframe,n,v):
